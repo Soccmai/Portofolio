c8e728de83887609c9e220e7aee30f0d8ead8084
Robert Csokmai <roboandspider@gmail.com>
2022-11-09 15:01:20 +0200

    added skills

--- scrollAnimation.js
+++ scrollAnimation.js
@@ -1,11 +1,14 @@
 const aboutMe = document.getElementById('infoRobert');
+const aboutMeTag = document.getElementById('li-about-me');
 const observerAboutMe = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry);
         if (entry.isIntersecting) {
             aboutMe.classList.add('show-left');
+            aboutMeTag.classList.add('hover-underline');
         }   else {
             aboutMe.classList.remove('show-left');
+            aboutMeTag.classList.remove('hover-underline');
         };
     });
 });
@@ -16,17 +19,42 @@ hiddenAboutMe.forEach((el) => observerAboutMe.observe(el));
 
 const projects = document.getElementById('project');
 const projTitle = document.getElementById('projTitle');
+const projTag = document.getElementById('li-projects');
 const observerProjects = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry);
         if (entry.isIntersecting) {
             projTitle.classList.add('show-left');
-            projects.classList.add('show-projects');
+            projects.classList.add('show-box');
+            projTag.classList.add('hover-underline');
         }   else {
             projTitle.classList.remove('show-left');
-            projects.classList.remove('show-projects');
+            projects.classList.remove('show-box');
+            projTag.classList.remove('hover-underline');
         };
     });
 });
 const hiddenProjects = document.querySelectorAll('.hidden-projects');
 hiddenProjects.forEach((el) => observerProjects.observe(el));
+
+
+
+const skill = document.getElementById('skills-icon');
+const skillTitle = document.getElementById('skills-title');
+const skillTag = document.getElementById('li-skills');
+const observerSkills = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            skillTitle.classList.add('show-left');
+            skill.classList.add('show-box');
+            skillTag.classList.add('hover-underline');
+        }   else {
+            skillTitle.classList.remove('show-left');
+            skill.classList.remove('show-box');
+            skillTag.classList.remove('hover-underline');
+        };
+    });
+});
+const hiddenSkills = document.querySelectorAll('.hidden-skills');
+hiddenSkills.forEach((el) => observerSkills.observe(el));
